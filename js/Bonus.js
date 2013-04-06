0df85379c3fca3ef52afefbcdebe1252fdf86cdc
winn77 <sanasack@gmail.com>
2013-04-07 00:17:29 +0200

    Global Re Indentation

--- js/Bonus.js
+++ js/Bonus.js
@@ -1,37 +1,37 @@
 //object creation bonus
 function Bonus(x,y,w,h){
  
-    this.posX =  x;
-	this.posY =  y;
-	this.width =  w;
-	this.height =  h;
-	this.imgVie = "";
-	this.imgCrash = "";
-	this.phase = 1;
-	
+  this.posX =  x;
+  this.posY =  y;
+  this.width =  w;
+  this.height =  h;
+  this.imgVie = "";
+  this.imgCrash = "";
+  this.phase = 1;
+  
 }
 
-    Bonus.prototype.setPosX = function(x) { 
-		this.posX=x;
-    }
-	
-    Bonus.prototype.setPosY = function(y) { 
-		this.posY=y;
-    }
-	
-    Bonus.prototype.getPosX = function() { 
-		return this.posX;
-    }
-	
-	Bonus.prototype.getPosY = function() { 
-		return this.posY;
-    }
-	
-	Bonus.prototype.getWidth = function() { 
-		return this.width;
-    }
-	
-	Bonus.prototype.getHeight = function() { 
-		return this.height;
-    }
-	
+Bonus.prototype.setPosX = function(x) { 
+  this.posX=x;
+}
+
+Bonus.prototype.setPosY = function(y) { 
+  this.posY=y;
+}
+
+Bonus.prototype.getPosX = function() { 
+  return this.posX;
+}
+
+Bonus.prototype.getPosY = function() { 
+  return this.posY;
+}
+
+Bonus.prototype.getWidth = function() { 
+  return this.width;
+}
+
+Bonus.prototype.getHeight = function() { 
+  return this.height;
+}
+  
